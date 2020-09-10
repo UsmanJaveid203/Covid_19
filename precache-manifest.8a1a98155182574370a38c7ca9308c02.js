25e9e61ba23d2ba52fd394e9782f4b74b367428a
Usman <webdevolper.185203@gmail.com>
2020-09-10 11:27:56 +0500

    Updates

---
+++ precache-manifest.8a1a98155182574370a38c7ca9308c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b72a2c0eecbcb6b1fa454a56b027032",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "74312f3bddcaa65df0d4",
+    "url": "/my-app/static/css/main.a3ae09fe.chunk.css"
+  },
+  {
+    "revision": "c01a704ae6cb7a57183a",
+    "url": "/my-app/static/js/2.0b5d207c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.0b5d207c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74312f3bddcaa65df0d4",
+    "url": "/my-app/static/js/main.47dc2d29.chunk.js"
+  },
+  {
+    "revision": "141ce6be05ccea4fc349",
+    "url": "/my-app/static/js/runtime-main.81bd623e.js"
+  }
+]);
